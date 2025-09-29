19b788112e10b6cb60224ea3ae996a7f31e65013
rashedul333 <rashedulislamsohan333@gmail.com>
2025-09-29 22:35:55 +0600

    new commit

---
+++ php/casting.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $number = 20;
+    $name = "Sohan";
+    $fnumber = 5.5;
+    var_dump((int)$number);
+    // echo "<br>";
+    var_dump((string)$name);
+    var_dump((float)$fnumber);
+    ?>
+</body>
+</html>
---
+++ php/var.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x = 10;
+    $y = "Sohan";
+    $z = 5.5;
+    $a = true;
+    var_dump($x, $y, $z, $a);
+    ?>
+
+</body>
+</html>
